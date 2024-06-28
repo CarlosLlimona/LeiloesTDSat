dfbb5862218a777f7a31fc0ccc593142f7e2120d
Carlos Llimona <carloscapecci@gmail.com>
2024-06-28 17:13:55 -0300

    Configurando classe conectaDAO com o banco de dados local

--- src/conectaDAO.java
+++ src/conectaDAO.java
@@ -5,16 +5,6 @@ import java.sql.SQLException;
 import javax.swing.JOptionPane;
 
 
-
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-
-/**
- *
- * @author Adm
- */
 public class conectaDAO {
     
     public Connection connectDB(){
@@ -22,9 +12,10 @@ public class conectaDAO {
         
         try {
         
-            conn = DriverManager.getConnection("jdbc:mysql://localhost/uc11?user=root&password=");
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/uc11","root","20281268");
             
-        } catch (SQLException erro){
+        } catch (ClassNotFoundException | SQLException erro){
             JOptionPane.showMessageDialog(null, "Erro ConectaDAO" + erro.getMessage());
         }
         return conn;
